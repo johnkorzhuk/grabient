298138b6fcaa3e41fb0aae144f65753d76ae3ef2
John Korzhuk <jkorzhuk@gmail.com>
2017-05-26 11:12:42 -0400

    turned angle into a PureComponent

--- src/components/Icons/AnglePrev.js
+++ src/components/Icons/AnglePrev.js
@@ -1,53 +1,60 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { Animate } from 'react-move'
 
-const AnglePrev = ({
-  angle,
-  color,
-  hovered,
-  editingAngle,
-  animationDuration
-}) => {
-  return (
-    <Animate
-      duration={animationDuration}
-      data={{
-        activeOpacity: editingAngle ? hovered ? 0 : 1 : hovered ? 1 : 0,
-        inactiveOpacity: editingAngle ? hovered ? 1 : 0 : hovered ? 0 : 1
-      }}
-    >
-      {data => {
-        return (
-          <svg
-            width='20'
-            height='20'
-            viewBox='0 0 20 20'
-            style={{
-              transform: `rotate(${angle}deg)`
-            }}
-          >
-            <path
-              d='M10 20C4.47715 20 0 15.52285 0 10S4.47715 0 10 0s10 4.47715 10 10-4.47715 10-10 10zm1-18H9v7h2V2z'
-              fillRule='nonzero'
-              fill={color}
-              fillOpacity={data.activeOpacity}
-            />
-            <path
-              d='M9 2.0619C5.0537 2.554 2 5.92037 2 10c0 4.41828 3.58172 8 8 8s8-3.58172 8-8c0-4.07962-3.0537-7.446-7-7.9381V9H9V2.0619zM10 20C4.47715 20 0 15.52285 0 10S4.47715 0 10 0s10 4.47715 10 10-4.47715 10-10 10z'
-              fillRule='nonzero'
-              fill={color}
-              fillOpacity={data.inactiveOpacity}
-            />
-          </svg>
-        )
-      }}
-    </Animate>
-  )
+class AnglePrev extends PureComponent {
+  shouldComponentUpdate (nextProps) {
+    return (
+      this.props.angle !== nextProps.angle ||
+      this.props.hovered !== nextProps.hovered ||
+      this.props.editingAngle !== nextProps.editingAngle
+    )
+  }
 
-  //  data={{
-  // activeOpacity: inverted ? 1 : 0,
-  // inactiveOpacity: inverted ? 0 : 1
-  // }}
+  render () {
+    const {
+      angle,
+      color,
+      hovered,
+      editingAngle,
+      animationDuration
+    } = this.props
+
+    return (
+      <Animate
+        duration={animationDuration}
+        data={{
+          activeOpacity: editingAngle ? hovered ? 0 : 1 : hovered ? 1 : 0,
+          inactiveOpacity: editingAngle ? hovered ? 1 : 0 : hovered ? 0 : 1
+        }}
+      >
+        {data => {
+          return (
+            <svg
+              width='20'
+              height='20'
+              viewBox='0 0 20 20'
+              style={{
+                transform: `rotate(${angle}deg)`
+              }}
+            >
+              <path
+                d='M10 20C4.47715 20 0 15.52285 0 10S4.47715 0 10 0s10 4.47715 10 10-4.47715 10-10 10zm1-18H9v7h2V2z'
+                fillRule='nonzero'
+                fill={color}
+                fillOpacity={data.activeOpacity}
+              />
+              <path
+                d='M9 2.0619C5.0537 2.554 2 5.92037 2 10c0 4.41828 3.58172 8 8 8s8-3.58172 8-8c0-4.07962-3.0537-7.446-7-7.9381V9H9V2.0619zM10 20C4.47715 20 0 15.52285 0 10S4.47715 0 10 0s10 4.47715 10 10-4.47715 10-10 10z'
+                fillRule='nonzero'
+                fill={color}
+                fillOpacity={data.inactiveOpacity}
+              />
+            </svg>
+          )
+        }}
+      </Animate>
+    )
+  }
 }
 
 export default AnglePrev
