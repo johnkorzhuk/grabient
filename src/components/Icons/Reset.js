eccfcb4fc99280c1166aff1ee20a587bd5539f65
John Korzhuk <jkorzhuk@gmail.com>
2017-06-26 11:08:15 -0400

    replaced trash icon

---
+++ src/components/Icons/Reset.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Reset = ({ color, hovered }) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Reset
--- src/components/Icons/Trash.js
+++ src/components/Icons/Trash.js
@@ -1,7 +1,4 @@
 import React, { PureComponent } from 'react'
-import TrashIconO from 'react-icons/lib/fa/trash-o'
-import TrashIcon from 'react-icons/lib/fa/trash'
-import { Animate } from 'react-move'
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -11,47 +8,58 @@ const Container = styled.div`
   align-items: center;
 `
 
+const TrashIcon = ({ color }) => {
+  return (
+    <svg
+      width='20'
+      height='20'
+      viewBox='0 0 20 20'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <g fill='none' fillRule='evenodd'>
+        <path fill={color} d='M0 4h20v2H0zM7 8h2v8H7V8zm4 0h2v8h-2V8z' />
+        <rect
+          stroke={color}
+          strokeWidth='2'
+          x='4'
+          y='1'
+          width='12'
+          height='18'
+          rx='3'
+        />
+      </g>
+    </svg>
+  )
+}
+
+const TrashIconO = ({ color }) => {
+  return (
+    <svg
+      width='20'
+      height='20'
+      viewBox='0 0 20 20'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M17 4h3v2h-3v11c0 1.656854-1.343146 3-3 3H6c-1.656854 0-3-1.343146-3-3V6H0V4h3V3c0-1.656854 1.343146-3 3-3h8c1.656854 0 3 1.343146 3 3v1zM5 4v2h10V4H5zm2 4v8h2V8H7zm4 0v8h2V8h-2z'
+        fill={color}
+        fillRule='evenodd'
+      />
+    </svg>
+  )
+}
+
 class Trash extends PureComponent {
   shouldComponentUpdate (nextProps) {
     return this.props.inverted !== nextProps.inverted
   }
 
   render () {
-    const { color, inverted, animationDuration } = this.props
-
+    const { color, inverted } = this.props
     return (
-      <Animate
-        duration={animationDuration}
-        data={{
-          default: inverted ? 1 : 0,
-          inverted: inverted ? 0 : 1
-        }}
-      >
-        {data => {
-          return (
-            <Container>
-              <TrashIcon
-                size={20}
-                color={color}
-                style={{
-                  position: 'absolute',
-                  left: 0,
-                  opacity: data.default
-                }}
-              />
-              <TrashIconO
-                size={20}
-                color={color}
-                style={{
-                  position: 'absolute',
-                  left: 0,
-                  opacity: data.inverted
-                }}
-              />
-            </Container>
-          )
-        }}
-      </Animate>
+      <Container>
+        {inverted ? <TrashIconO color={color} /> : <TrashIcon color={color} />}
+      </Container>
     )
   }
 }
