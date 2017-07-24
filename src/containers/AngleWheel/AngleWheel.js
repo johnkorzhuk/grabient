595c80f60918ad64a2b929804caaa8979ef98ab2
John Korzhuk <jkorzhuk@gmail.com>
2017-07-24 10:29:16 -0400

    Added omg for twitter card

--- src/containers/AngleWheel/AngleWheel.js
+++ src/containers/AngleWheel/AngleWheel.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import styled from 'styled-components'
-import { Animate } from 'react-move'
-import Wheelpng from './../../wheel.png'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+import { Animate } from 'react-move';
+import Wheelpng from './../../wheel.png';
 
-import {
-  updateGradientAngle,
-  toggleEditing,
-  updateEditingAngle
-} from './../../store/gradients/actions'
+import { updateGradientAngle, toggleEditing, updateEditingAngle } from './../../store/gradients/actions';
 
-import { Arrow, Close } from './../../components/Icons/index'
-import { Button } from './../../components/Common/index'
-import { TextLG } from './../../components/Common/Typography'
+import { Arrow, Close } from './../../components/Icons/index';
+import { Button } from './../../components/Common/index';
+import { TextLG } from './../../components/Common/Typography';
 
 const AreaContainer = styled.div`
   width: 100%;
@@ -21,7 +17,7 @@ const AreaContainer = styled.div`
   justify-content: center;
   align-items: center;
   margin-top: -298px;
-`
+`;
 
 const Container = styled.div`
   position: relative;
@@ -31,16 +27,16 @@ const Container = styled.div`
   background-size: 220px auto;
   background-repeat: no-repeat;
   background-position: center;
-`
+`;
 
 const Background = styled.div`
   height: 100%;
   border-radius: 15px;
   position: absolute;
   width: 100%;
-  border: 1px solid  #000;
+  border: 1px solid #000;
   background-color: #000;
-`
+`;
 
 const AngleRef = styled.div`
   position: absolute;
@@ -48,7 +44,7 @@ const AngleRef = styled.div`
   left: 50%;
   height: 2px;
   width: 2px;
-`
+`;
 
 const TextContainer = styled.div`
   display: flex;
@@ -62,7 +58,7 @@ const TextContainer = styled.div`
 
   height: 85px;
   width: 85px;
-`
+`;
 
 const TextValue = TextLG.extend`
   color: white;
@@ -75,26 +71,26 @@ const TextValue = TextLG.extend`
 
   &::-webkit-inner-spin-button,
   ::-webkit-outer-spin-button {
-    -webkit-appearance: none; 
-    margin: 0; 
+    -webkit-appearance: none;
+    margin: 0;
   }
 
   &:focus {
     outline: none;
   }
-`.withComponent('input')
+`.withComponent('input');
 
 const Degree = TextLG.extend`
   color: white;
-  position: absolute
-`
+  position: absolute;
+`;
 
 const CloseButton = Button.extend`
   position: absolute;
   top: 18px;
   right: 18px;
   z-index: 17;
-`
+`;
 
 const ArrowContainer = styled.div`
   position: absolute;
@@ -104,182 +100,171 @@ const ArrowContainer = styled.div`
   left: 47.8%;
   top: 47.8%;
   cursor: pointer;
-`
+`;
 
 const origState = {
   cursorUpdatingAngle: true,
   updatingText: false,
   cursorUpdatingAngleAccurately: false
-}
+};
 class AngleWheel extends Component {
-  state = origState
+  state = origState;
 
-  componentWillReceiveProps (nextProps) {
+  componentWillReceiveProps(nextProps) {
     if (nextProps.editing) {
       setTimeout(() => {
         if (this.input) {
-          this.input.focus()
+          this.input.focus();
         }
-      }, this.props.transitionDuration + 50)
+      }, this.props.transitionDuration + 50);
     } else if (!nextProps.editing) {
-      this.setState(origState)
+      this.setState(origState);
     }
   }
 
-  shouldComponentUpdate (nextProps, nextState) {
-    if (this.props.angle !== nextProps.angle) return true
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.props.angle !== nextProps.angle) return true;
     if (this.state.cursorUpdatingAngle !== nextState.cursorUpdatingAngle) {
-      return true
+      return true;
     }
     if (this.props.editing !== nextProps.editing) {
-      return true
+      return true;
     }
-    return false
+    return false;
   }
 
   _handleMouseLeave = () => {
-    const { toggleEditing, id } = this.props
-    this.updateActualAngle()
-    toggleEditing(id)
+    const { toggleEditing, id } = this.props;
+    this.updateActualAngle();
+    toggleEditing(id);
     this.setState(() => ({
       cursorUpdatingAngle: false,
       updatingText: false
-    }))
-  }
+    }));
+  };
 
   _handleMouseMove = e => {
-    const {
-      cursorUpdatingAngle,
-      updatingText,
-      cursorUpdatingAngleAccurately
-    } = this.state
-    const { updateEditingAngle } = this.props
+    const { cursorUpdatingAngle, updatingText, cursorUpdatingAngleAccurately } = this.state;
+    const { updateEditingAngle } = this.props;
     if (cursorUpdatingAngle && !updatingText) {
-      let angle = this.getAngle(e.nativeEvent.offsetX, e.nativeEvent.offsetY)
-      if (!cursorUpdatingAngleAccurately) angle = this.checkCommonAngles(angle)
-      if (angle === 360) angle = 0
-      updateEditingAngle(angle)
+      let angle = this.getAngle(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+      if (!cursorUpdatingAngleAccurately) angle = this.checkCommonAngles(angle);
+      if (angle === 360) angle = 0;
+      updateEditingAngle(angle);
     }
-  }
+  };
 
   _handleKeyEnter = e => {
     if (e.which === 13) {
-      this.updateActualAngle()
-      this.toggleEditing()
-      this.setState(origState)
+      this.updateActualAngle();
+      this.toggleEditing();
+      this.setState(origState);
     }
-  }
+  };
 
   _handleInputChange = e => {
-    let angle = e.target.value
+    let angle = e.target.value;
     if (!isNaN(angle)) {
       if (angle > 359) {
-        angle = 0
-        this.input.value = angle
+        angle = 0;
+        this.input.value = angle;
       }
-      if (angle < 0) angle = 360 - Math.abs(angle % 360)
-      this._handleInputChange.lastValid = angle
-      this.updateEditingAngle(angle)
+      if (angle < 0) angle = 360 - Math.abs(angle % 360);
+      this._handleInputChange.lastValid = angle;
+      this.updateEditingAngle(angle);
     } else {
-      this.updateEditingAngle('')
+      this.updateEditingAngle('');
     }
-  }
+  };
 
   _handleInputClick = () => {
-    this.setState(() => ({ cursorUpdatingAngle: false }))
-  }
+    this.setState(() => ({ cursorUpdatingAngle: false }));
+  };
 
   _handleClick = e => {
-    const { cursorUpdatingAngle } = this.state
+    const { cursorUpdatingAngle } = this.state;
 
     if (cursorUpdatingAngle) {
-      this.updateActualAngle()
-      this.toggleEditing()
-      this.setState(origState)
+      this.updateActualAngle();
+      this.toggleEditing();
+      this.setState(origState);
     } else {
-      this.updateEditingAngle(
-        this.getAngle(e.nativeEvent.offsetX, e.nativeEvent.offsetY)
-      )
+      this.updateEditingAngle(this.getAngle(e.nativeEvent.offsetX, e.nativeEvent.offsetY));
     }
-  }
+  };
 
   _handleArrowClick = () => {
-    this.setState(() => ({ cursorUpdatingAngle: true }))
-  }
+    this.setState(() => ({ cursorUpdatingAngle: true }));
+  };
 
   _handleClose = () => {
-    this.toggleEditing(null)
-  }
+    this.toggleEditing(null);
+  };
 
   _handleMouseDown = e => {
-    e.preventDefault()
-    const { cursorUpdatingAngle } = this.state
+    e.preventDefault();
+    const { cursorUpdatingAngle } = this.state;
     if (cursorUpdatingAngle) {
       this.setState({
         cursorUpdatingAngleAccurately: true
-      })
+      });
     }
-  }
+  };
 
   _handleTextContainerClick = () => {
-    this.input.select()
+    this.input.select();
+  };
+
+  toggleEditing() {
+    const { id, toggleEditing } = this.props;
+    toggleEditing(id);
   }
 
-  toggleEditing () {
-    const { id, toggleEditing } = this.props
-    toggleEditing(id)
+  updateEditingAngle(angle) {
+    const { updateEditingAngle } = this.props;
+    updateEditingAngle(angle);
   }
 
-  updateEditingAngle (angle) {
-    const { updateEditingAngle } = this.props
-    updateEditingAngle(angle)
+  updateActualAngle() {
+    const { updateGradientAngle, id, angle, origAngle } = this.props;
+    const newAngle = isNaN(angle) ? origAngle : angle;
+    updateGradientAngle(id, newAngle);
   }
 
-  updateActualAngle () {
-    const { updateGradientAngle, id, angle, origAngle } = this.props
-    const newAngle = isNaN(angle) ? origAngle : angle
-    updateGradientAngle(id, newAngle)
+  checkCommonAngles(angle) {
+    if (angle <= 10 || angle >= 350) return 0;
+    else if (angle >= 35 && angle <= 55) return 45;
+    else if (angle >= 80 && angle <= 100) return 90;
+    else if (angle >= 125 && angle <= 145) return 135;
+    else if (angle >= 170 && angle <= 190) return 180;
+    else if (angle >= 215 && angle <= 235) return 225;
+    else if (angle >= 260 && angle <= 280) return 270;
+    else if (angle >= 305 && angle <= 325) return 315;
+    return angle;
   }
 
-  checkCommonAngles (angle) {
-    if (angle <= 10 || angle >= 350) return 0
-    else if (angle >= 35 && angle <= 55) return 45
-    else if (angle >= 80 && angle <= 100) return 90
-    else if (angle >= 125 && angle <= 145) return 135
-    else if (angle >= 170 && angle <= 190) return 180
-    else if (angle >= 215 && angle <= 235) return 225
-    else if (angle >= 260 && angle <= 280) return 270
-    else if (angle >= 305 && angle <= 325) return 315
-    return angle
+  getAngle(offsetX, offsetY) {
+    const boxCenter = this.getBoxCenter();
+    let angle = Math.atan2(offsetX - boxCenter[0], -(offsetY - boxCenter[1])) * (180 / Math.PI);
+    if (angle < 0) angle += 360;
+    return Math.round(angle);
   }
 
-  getAngle (offsetX, offsetY) {
-    const boxCenter = this.getBoxCenter()
-    let angle =
-      Math.atan2(offsetX - boxCenter[0], -(offsetY - boxCenter[1])) *
-      (180 / Math.PI)
-    if (angle < 0) angle += 360
-    return Math.round(angle)
+  getBoxCenter() {
+    return [this.box.offsetLeft + this.box.offsetWidth / 2, this.box.offsetTop + this.box.offsetHeight / 2];
   }
 
-  getBoxCenter () {
-    return [
-      this.box.offsetLeft + this.box.offsetWidth / 2,
-      this.box.offsetTop + this.box.offsetHeight / 2
-    ]
+  getLeft(angle) {
+    const length = angle.toString().length;
+
+    if (length === 3) return 65;
+    else if (length === 2) return 59;
+    else return 50;
   }
 
-  getLeft (angle) {
-    const length = angle.toString().length
-
-    if (length === 3) return 65
-    else if (length === 2) return 59
-    else return 50
-  }
-
-  render () {
-    const { cursorUpdatingAngle } = this.state
-    const { transitionDuration, editing, angle, ...props } = this.props
+  render() {
+    const { cursorUpdatingAngle } = this.state;
+    const { transitionDuration, editing, angle, ...props } = this.props;
     return (
       <Animate
         data={{
@@ -308,13 +293,13 @@ class AngleWheel extends Component {
               >
                 <AngleRef
                   innerRef={node => {
-                    this.box = node
+                    this.box = node;
                   }}
                 />
               </Container>
 
-              <CloseButton onClick={this._handleClose} title='Exit'>
-                <Close color='white' size={15} />
+              <CloseButton onClick={this._handleClose} title="Exit">
+                <Close color="white" size={15} />
               </CloseButton>
 
               <TextContainer
@@ -324,18 +309,19 @@ class AngleWheel extends Component {
                 }}
               >
                 <TextValue
-                  className='TextValue'
+                  className="TextValue"
                   autoFocus
                   innerRef={node => {
-                    this.input = node
+                    this.input = node;
                   }}
                   onFocus={e => e.target.select()}
                   onClick={this._handleInputClick}
                   onKeyDown={this._handleKeyEnter}
-                  type='number'
+                  type="number"
                   value={angle}
                   onChange={this._handleInputChange}
-                /><Degree
+                />
+                <Degree
                   style={{
                     left: this.getLeft(angle)
                   }}
@@ -354,10 +340,10 @@ class AngleWheel extends Component {
                 <Arrow />
               </ArrowContainer>
             </AreaContainer>
-          )
+          );
         }}
       </Animate>
-    )
+    );
   }
 }
 
@@ -365,9 +351,7 @@ export default connect(
   ({ gradients: { editingAngle } }, { id, angle }) => ({
     // eslint-disable-next-line eqeqeq
     editing: id == editingAngle.id,
-    angle: !isNaN(editingAngle.angle)
-      ? editingAngle.angle === null ? angle : editingAngle.angle
-      : angle,
+    angle: !isNaN(editingAngle.angle) ? (editingAngle.angle === null ? angle : editingAngle.angle) : angle,
     origAngle: angle
   }),
   {
@@ -375,4 +359,4 @@ export default connect(
     toggleEditing,
     updateEditingAngle
   }
-)(AngleWheel)
+)(AngleWheel);
