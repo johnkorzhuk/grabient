42178e25dca92a51c748b3cae25fa01c3d8a2cf6
John Korzhuk <jkorzhuk@gmail.com>
2020-05-04 15:14:53 -0400

    added rora ad

--- src/components/GradientList/GradientList.js
+++ src/components/GradientList/GradientList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-
+import roraAd from "../../assets/rora-grabient@2x.png";
 import { GradientCard } from "./../../containers/index";
 
 const Container = styled.ul`
@@ -13,17 +13,28 @@ const Container = styled.ul`
   }
 `;
 
-const GradientList = ({ gradients }) =>
-  <Container>
-    {gradients.map((item, index) =>
-      <GradientCard
-        key={item.id}
-        gradient={item}
-        index={index}
-        width="100%"
-        id={item.id}
-      />
-    )}
-  </Container>;
+const RoraAd = styled.img`
+  padding: 20px;
+  width: 100%;
+`;
+
+const GradientList = ({ gradients }) => (
+  <div>
+    <a href="https://rora.co/">
+      <RoraAd src={roraAd} alt="rora ad" />
+    </a>
+    <Container>
+      {gradients.map((item, index) => (
+        <GradientCard
+          key={item.id}
+          gradient={item}
+          index={index}
+          width="100%"
+          id={item.id}
+        />
+      ))}
+    </Container>
+  </div>
+);
 
 export default GradientList;
