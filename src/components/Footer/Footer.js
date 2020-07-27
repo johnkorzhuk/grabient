159f6ac0ca742fca26b11943421b92acc5da57a5
John Korzhuk <jkorzhuk@gmail.com>
2020-07-27 18:15:46 -0400

    added carbon ads

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,22 +1,38 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import john from './../../assets/john.png';
-import eddie from './../../assets/eddie.png';
+import john from "./../../assets/john.png";
+import eddie from "./../../assets/eddie.png";
 
-import { TextXS, TextSM } from './../Common/Typography';
-import { UnfoldLogo } from './../Common/index';
-import { ActionGroupItem, ActionGroupItemContainer } from './../index';
+import { TextXS, TextSM } from "./../Common/Typography";
+import { UnfoldLogo } from "./../Common/index";
+import { ActionGroupItem, ActionGroupItemContainer } from "./../index";
 
-const PhotoText = TextSM.extend`margin-left: 7px;`;
+const PhotoText = TextSM.extend`
+  margin-left: 7px;
+`;
 
-const Container = styled.footer`
+const PrimaryContainer = styled.footer`
   margin: 0 auto 30px;
   padding: 0 20px;
   max-width: 1100px;
+`;
+
+const AdContainer = styled.div`
+  margin-top: 24px;
+`;
+
+const CarbonAd = styled.div`
+  margin-left: auto;
+
+  @media (max-width: 620px) {
+    margin-right: auto;
+  }
+`;
+
+const Container = styled.div`
   display: flex;
   justify-content: space-between;
-
   align-items: center;
   flex-wrap: wrap;
 
@@ -25,7 +41,7 @@ const Container = styled.footer`
   }
 `;
 
-const John = () =>
+const John = () => (
   <ActionGroupItem
     pretext="Development:"
     href="https://twitter.com/johnkorzhuk"
@@ -33,14 +49,15 @@ const John = () =>
       order: -1
     }}
     style={{
-      cursor: 'pointer'
+      cursor: "pointer"
     }}
   >
     <PhotoText>@johnkorzhuk</PhotoText>
     <img src={john} alt="john" />
-  </ActionGroupItem>;
+  </ActionGroupItem>
+);
 
-const Eddie = () =>
+const Eddie = () => (
   <ActionGroupItem
     pretext="Design:"
     href="https://twitter.com/lobanovskiy"
@@ -48,28 +65,39 @@ const Eddie = () =>
       order: -1
     }}
     style={{
-      cursor: 'pointer'
+      cursor: "pointer"
     }}
   >
     <PhotoText>@lobanovskiy</PhotoText>
     <img src={eddie} alt="eddie" />
-  </ActionGroupItem>;
+  </ActionGroupItem>
+);
 
-const Footer = () =>
-  <Container>
-    <ActionGroupItemContainer>
-      <John />
-      <Eddie />
-    </ActionGroupItemContainer>
+const Footer = () => (
+  <PrimaryContainer>
+    <Container>
+      <ActionGroupItemContainer>
+        <John />
+        <Eddie />
+      </ActionGroupItemContainer>
 
-    <ActionGroupItemContainer>
-      <ActionGroupItem href="https://www.unfold.co">
-        <TextXS>
-          ©{new Date().getFullYear()} Grabient by
-        </TextXS>
-        <UnfoldLogo />
-      </ActionGroupItem>
-    </ActionGroupItemContainer>
-  </Container>;
+      <ActionGroupItemContainer>
+        <ActionGroupItem href="https://www.unfold.co">
+          <TextXS>©{new Date().getFullYear()} Grabient by</TextXS>
+          <UnfoldLogo />
+        </ActionGroupItem>
+      </ActionGroupItemContainer>
+    </Container>
+    <AdContainer>
+      <script
+        async
+        type="text/javascript"
+        src="//cdn.carbonads.com/carbon.js?serve=CE7IK53E&placement=wwwgrabientcom"
+        id="_carbonads_js"
+      />
+      <CarbonAd id="carbonads"></CarbonAd>
+    </AdContainer>
+  </PrimaryContainer>
+);
 
 export default Footer;
