da80189fb6a0707e1dc5ed1a2e1ff3aeab506a45
John Korzhuk <jkorzhuk@gmail.com>
2025-12-02 18:54:39 -0500

    Add COLORS constant with 16 standard color hex values

--- apps/user-application/src/lib/constants.ts
+++ apps/user-application/src/lib/constants.ts
@@ -1,1 +1,20 @@
-export const DEFAULT_PAGE_LIMIT = 20;
+export const DEFAULT_PAGE_LIMIT = 20 as const;
+
+export const COLORS = {
+    white: "#ffffff",
+    gray: "#808080",
+    black: "#000000",
+    brown: "#8b4513",
+    red: "#ff0000",
+    orange: "#ff8000",
+    yellow: "#ffff00",
+    lime: "#80ff00",
+    green: "#00aa00",
+    teal: "#008080",
+    cyan: "#00ffff",
+    blue: "#0080ff",
+    navy: "#000080",
+    purple: "#8000ff",
+    magenta: "#ff00ff",
+    pink: "#ff80c0",
+} as const;
